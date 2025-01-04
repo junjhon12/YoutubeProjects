df269d442f8476ffaf637ad45107ab49c369bf26
junjhon12 <qle17@student.gsu.edu>
2025-01-04 16:41:14 -0500

    phone view almost done

--- project04/src/app/page.tsx
+++ project04/src/app/page.tsx
@@ -2,100 +2,74 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className=" mobile:mx-5 mobile:mt-5">
+      <div className="navigation mobile:flex mobile:place-content-between mobile:items-center mobile:mb-5">
+        <div>
+          <Image src="/logo.svg" alt="logo" width={50} height={50} />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div>
+        <Image src="/icon-menu.svg" alt="logo" width={50} height={50} className="hover:cursor-pointer"/>
+        </div>
+      </div>
+
+      <div className="container1 mobile:flex mobile:flex-col mb-20">
+        <div>
+          <Image src="/image-web-3-mobile.jpg" alt="picture1" width={1000} height={50} className="mb-5"/>
+        </div>
+        <div className="font-extrabold text-4xl mb-5">The Bright Future of Web 3.0?</div>
+        <div className="mb-5 text-Darkgrayishblue">We dive into the next evolution of the web that claims t oput the power of the platforms back into the hands of the people. But is it really fulfilling its promise?</div>
+        <div className="">
+          <button type="button" className="bg-SoftRed py-3 px-10 font-bold tracking-widest ">Read More</button>
+        </div>
+      </div>
+
+      <div className="container2 mb-14">
+        <div className="bg-Verydarkblue px-5 py-5">
+          <div className="text-SoftOrange font-bold text-2xl mb-5">New</div>
+          <div className="text-OffWhite">
+            <div className="mb-5 border-b-2 border-Darkgrayishblue pb-5">
+              <div className="title">Hydrogen VS Electric Cars</div>
+              <div className="question text-Darkgrayishblue">Will hydrogen-fueled cars ever catch up to EVs?</div>
+            </div>
+            <div className="mb-5 border-b-2 border-Darkgrayishblue pb-5">
+            <div className="title">The Downsides of AI Artistry</div>
+            <div className="question text-Darkgrayishblue">What are the possible adverse effects of on-demand AI image generation?</div>
+            </div>
+            <div>
+            <div className="title">Is VC Funding Drying Up?</div>
+            <div className="question text-Darkgrayishblue">Private funding by VC firms is down 50% YOY. We take a look at what that means.</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container3 mb-28">
+        <div className="flex mb-5">
+          <div className="flex-0"><Image src="/image-retro-pcs.jpg" alt="logo" width={110} height={100} /></div>
+          <div className="flex flex-col ml-4 flex-1 justify-between">
+            <div className="font-bold text-SoftRed text-3xl">01</div>
+            <div className="font-extrabold text-base">Reviving Retro PCs</div>
+            <div className="text-Darkgrayishblue">What happens when old PCs are given modern upgrades?</div>
+          </div>
+        </div>
+        <div className="flex mb-5">
+          <div className="flex-0">
+            <Image src="/image-top-laptops.jpg" alt="logo" width={110} height={100} />
+            </div>
+          <div className="flex flex-col ml-4 flex-1 justify-between">
+            <div className="font-bold text-SoftRed text-3xl">02</div>
+            <div className="font-extrabold text-base">Top 10 Laptops of 2022</div>
+            <div className="text-Darkgrayishblue">Our best picks for vaious needs and budgets.</div>
+          </div>
+        </div>
+        <div className="flex">
+          <div className="flex-0"><Image src="/image-gaming-growth.jpg" alt="logo" width={110} height={100} /></div>
+          <div className="flex flex-col ml-4 flex-1 justify-between">
+            <div className="font-bold text-SoftRed text-3xl">03</div>
+            <div className="font-extrabold text-base">The Growth of Gaming</div>
+            <div className="text-Darkgrayishblue">How the pandemic has spiked fresh opportunities.</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- project04/tailwind.config.ts
+++ project04/tailwind.config.ts
@@ -11,6 +11,16 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        SoftOrange: 'hsl(35, 77%, 62%)',
+        SoftRed: 'hsl(5, 85%, 63%)',
+        OffWhite: 'hsl(36, 100%, 99%)',
+        Grayishblue: 'hsl(233, 8%, 79%)',
+        Darkgrayishblue: 'hsl(236, 13%, 42%)',
+        Verydarkblue: 'hsl(240, 100%, 5%)',
+      },
+      screens: {
+        mobile: "375px",
+        desktop: "1440px",
       },
     },
   },
