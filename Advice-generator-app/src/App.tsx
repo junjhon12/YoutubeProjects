0485a445b893aea414981e9acf4fb86b18c94e01
quocIntern <quocle@thaddeus.org>
2025-05-10 00:12:40 -0400

    Setup React, Tailwind CSS, and TypeScript

---
+++ Advice-generator-app/src/App.tsx
@@ -0,0 +1,43 @@
+import { Fragment, useCallback, useEffect, useState } from 'react';
+import React from 'react';
+
+function App() {
+  const[advice, setAdvice] = useState(null);
+  const[loading, setLoading] = useState(true);
+  const[error, setError] = useState(null);
+
+  //Fetch the info from API
+  const fetchAdvice = useCallback(async () => {
+    setLoading(true); 
+    setError(null); //Clear for new advice
+    try {
+      const advice = await fetch('https://api.adviceslip.com/advice');
+      if (!Response.ok) {
+        throw new Error('HTTP error! status: ${response.status}');
+      }
+      const data = await advice.json();
+      setAdvice(data);
+    } catch (e) {
+      setError(e.message);
+      setAdvice(null);
+      console.error("Failed to fetch advice:", e);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchAdvice();
+  }, [fetchAdvice]);
+
+  const handleAdvice = () => {
+    fetchAdvice();
+  };
+  return (
+    <Fragment className="min-h-screen bg-blue">
+
+    </Fragment>
+  )
+}
+
+export default App
