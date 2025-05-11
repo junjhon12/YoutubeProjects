ad0378f6fbe57a888ef6be55e94bfb3c0bcda59b
quocIntern <quocle@thaddeus.org>
2025-05-10 22:54:31 -0400

    Update App.tsx

--- API-Advice-Gen/src/App.tsx
+++ API-Advice-Gen/src/App.tsx
@@ -1,33 +1,44 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [advice, setAdvice] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const API_URL = "https://api.adviceslip.com/advice";
+
+  const fetchJoke = async () => {
+    setLoading(true);
+    setError(null);
+    setAdvice(null);
+
+    try {
+      const response = await fetch(API_URL);
+
+      if (!response.ok) {
+        throw new Error('Error! Status: ${response.status}');
+      }
+
+      const data = await response.json();
+
+      setAdvice({ setup: data.setup, advice: data.advice});
+
+    } catch (err) {
+      setError(err.message);
+      console.error("Failed to fetch:", error)
+    } finally {
+      setLoading(false);
+    }
+  };
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='App'>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
